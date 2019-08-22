9b03cb1e881c0c19c8884048261820f712dfa202
Julia Kim <kimjulia313@gmail.com>
2019-08-22 08:47:38 -0500

    Define GET request for example route

--- server/index.js
+++ server/index.js
@@ -7,4 +7,13 @@ const port = 1004;
 
 app.use(express.static('dist'));
 
+app.get('/items', (req, res) => {
+  db.getOneItem('media', {'productName': 'Harry Potter\'s Wand'}, (err, docs) => {
+    if (err) {
+      return err;
+    }
+    res.send(docs);
+  });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
