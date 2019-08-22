7f49fb7c55288285b233290bb730e0e04817df1b
Julia Kim <kimjulia313@gmail.com>
2019-08-22 14:21:01 -0500

    Define function to retrieve example data on client-side and render to page

--- client/components/MediaContainer.jsx
+++ client/components/MediaContainer.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class MediaContainer extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      images: []
+    };
+  }
+
+  getItem() {
+    axios.get('/items')
+      .then(({ data }) => {
+        this.setState({images: data[0].images});
+      });
+  }
+
+  componentDidMount() {
+    this.getItem();
   }
 
   render() {
-    return (<div>Hello from the Media Container~</div>);
+    return (
+      <>
+        {this.state.images.map(image => <img src={image}/>)}
+      </>
+    );
   }
 }
 
