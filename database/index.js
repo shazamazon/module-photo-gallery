bbcd2d1600e3806aa895a0a3cdd88e2e87a00b36
Julia Kim <kimjulia313@gmail.com>
2019-08-22 08:38:05 -0500

    Implement function to query database

--- database/index.js
+++ database/index.js
@@ -7,4 +7,10 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('connected to database'));
 
-module.exports = { db };
+const findItem = (name, query, callback) => {
+  db.db.collection(name, (err, collection) => {
+    collection.find(query).toArray(callback);
+  });
+};
+
+module.exports = { db, findItem };
