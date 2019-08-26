426745c9c8184f8e7f2f37c87990e8a5203c9b1e
jxkim <51859266+jxkim@users.noreply.github.com>
2019-08-26 18:41:10 -0500

    Add data and seed database (#8)
    * Add data
    
    * Implement function to seed database

--- database/index.js
+++ database/index.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
+const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/shazamazon', {useNewUrlParser: true});
 
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('connected to database'));
+
+const mediaSchema = new mongoose.Schema({
+  ProductId: Number,
+  ItemName: String,
+  Photo: Array,
+  Video: String
+}, {collection: 'media'});
+
+const media = mongoose.model('media', mediaSchema);
+
+const seedDatabase = () => {
+  media.collection.insertMany(data, (err, docs) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log('Multiple documents inserted');
+    }
+  });
+};
 
 const findItem = (name, query, callback) => {
   db.db.collection(name, (err, collection) => {
@@ -13,4 +32,4 @@ const findItem = (name, query, callback) => {
   });
 };
 
-module.exports = { db, findItem };
+module.exports = { db, seedDatabase, findItem };
