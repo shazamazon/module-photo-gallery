33ed7a99bbfebb53cf89dc7defb52f39f02fe832
Julia Kim <kimjulia313@gmail.com>
2019-08-26 14:41:32 -0500

    Change main image on hover over thumbnail

--- client/components/AdditionalMedia.jsx
+++ client/components/AdditionalMedia.jsx
@@ -5,7 +5,7 @@ import Image from './Image.jsx';
 const AdditionalMedia = (props) => {
   return (
     <div id='gall_additional'>
-      {props.images.map(image => <Image image={image} />)}
+      {props.images.map(image => <Image image={image} selectView={props.selectView} />)}
     </div>
   );
 };
