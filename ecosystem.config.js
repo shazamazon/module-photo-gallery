c271c5fecf7be55f33caa30b904b1e4f13441cba
Julia Kim <kimjulia313@gmail.com>
2019-09-03 16:30:37 -0500

    Correct config path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       key: '~/.ssh/FEC.pem',
       ref: 'origin/master',
       repo: 'git@github.com:shazamazon/module-photo-gallery',
-      path: '/home/ubuntu/module-photo-gallery',
+      path: '/home/ubuntu/',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
