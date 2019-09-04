93ee3f52985c1cf53c6cf1b7375b7c686a07bc32
Julia Kim <kimjulia313@gmail.com>
2019-09-03 22:29:38 -0500

    Create portal to append modal to separate DOM node

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const port = 8369;
 
 app.use(express.static('dist'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
